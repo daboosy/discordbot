851141ab9e6fd4520b639ba0453c308b28d14b36
Daboosy <72225518+daboosy@users.noreply.github.com>
2020-10-16 00:41:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -104,3 +104,53 @@ client.on("message", message => {
   }
 });
 
+client.on("message", message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+    if (message.author.bot) return;
+    if (!message.channel.guild)
+      return message.reply("** cmd l servers**");
+
+    if (!message.guild.member(message.author).hasPermission("KICK_MEMBERS"))
+      return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
+    if (!message.guild.member(client.user).hasPermission("KICK_MEMBERS"))
+      return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+    let user = message.mentions.users.first();
+    let reason = message.content
+      .split(" ")
+      .slice(2)
+      .join(" ");
+    if (message.mentions.users.size < 1) return message.reply("**tagi l3abed **");
+    if (!reason) return message.reply("**ekteb sbab tared**");
+    if (!message.guild.member(user).kickable)
+      return message.reply(
+        "**najmch ntared wehed ando role a3la meny**"
+      );
+    if (
+      message.mentions.members.first().highestRole.position >=
+      message.member.highestRole.position
+    )
+      return message.channel.send("tnajmch tared wehed ando role a9wa menk!");
+
+    message.guild.member(user).kick();
+
+    const kickembed = new Discord.RichEmbed()
+      .setAuthor(`KICKED!`, user.displayAvatarURL)
+      .setColor("RANDOM")
+      .setTimestamp()
+      .addField("**ly tahchelou:**", "**[ " + `${user.tag}` + " ]**")
+      .addField("**ly tardou:**", "**[ " + `${message.author.tag}` + " ]**")
+      .addField("**sbab:**", "**[ " + `${reason}` + " ]**");
+    message.channel.send({
+      embed: kickembed
+    });
+  }
+});
+
