9f46a2ea9cc5a1921f3b3a066e097edd5823b58e
Daboosy <72225518+daboosy@users.noreply.github.com>
2020-10-16 00:44:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -173,3 +173,26 @@ client.on("message", message => {
       .setFooter(message.author.username, message.client.avatarURL);
     message.channel.send(bot);
   }
+
+  client.on('message', message => {
+if(message.content.startsWith(prefix +"server")){
+if(!message.channel.guild) return message.reply(' ');
+const millis = new Date().getTime() - message.guild.createdAt.getTime();
+const now = new Date();
+dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
+const days = millis / 1000 / 60 / 60 / 24;
+let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+var embed  = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.addField("** Server ID:**", message.guild.id,true)
+.addField("** Created On**", message.guild.createdAt.toLocaleString(),true)
+.addField("** Owned by**",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
+.addField("** Members**",`[${message.guild.memberCount}]`,true)
+.addField('** Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text  |  Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
+.addField("**🌐 Others **" , message.guild.region,true)
+.addField("** Roles **",`**[${message.guild.roles.size}]** Role `,true)
+message.channel.sendEmbed(embed)
+ 
+}
+});
